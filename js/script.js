371c328da0a218ed400ed9a6057457ca103ac1cd
Pratimesh Shinde <pratimeshshinde121@gmail.com>
2022-12-12 20:41:34 +0530

    Falcon,FalconHeavy,Dragon Page Done

--- js/script.js
+++ js/script.js
@@ -1,10 +1,62 @@
 const btn = document.getElementById('menu-btn');
 const overlay = document.getElementById('overlay');
+const menu = document.getElementById('mobile-menu');
+const counters = document.querySelectorAll('.counter');
+let scrollStarted = false;
 
 btn.addEventListener('click', navToggle);
+document.addEventListener('scroll', scrollPage);
 
 function navToggle() {
   btn.classList.toggle('open');
   overlay.classList.toggle('overlay-show');
   document.body.classList.toggle('stop-scrolling');
+  menu.classList.toggle('show-menu');
+}
+
+function scrollPage() {
+  const scrollPos = window.scrollY;
+
+  if (scrollPos > 100 && !scrollStarted) {
+    countUp();
+    scrollStarted = true;
+  } else if (scrollPos < 100 && scrollStarted) {
+    reset();
+    scrollStarted = false;
+  }
+}
+
+function countUp() {
+  counters.forEach((counter) => {
+    counter.innerText = '0';
+
+    const updateCounter = () => {
+      //  Get Count Target
+      const target = +counter.getAttribute('data-target');
+      // Get current Counter Value
+      const c = +counter.innerText;
+
+      // Create an increment
+      const increment = target / 100;
+
+      // If counter is less than the target then add increment
+
+      if (c < target) {
+        // Round Up & set the counter value
+        counter.innerText = `${Math.ceil(c + increment)}`;
+
+        setTimeout(updateCounter, 75);
+      } else {
+        counter.innerText = target;
+      }
+    };
+
+    updateCounter();
+  });
+}
+
+function reset() {
+  counters.forEach((counter) => {
+    counter.innerHTML = 0;
+  });
 }
